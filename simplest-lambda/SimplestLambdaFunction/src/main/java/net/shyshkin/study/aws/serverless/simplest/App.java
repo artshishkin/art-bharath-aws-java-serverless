cf45cdebece5fa6df1e5275fcb9df3a2576c2d33
Art <d.art.shishkin@gmail.com>
2021-12-14 22:42:39 +0200

    34.3 Custom build process - classes and dependencies (like default) (#1)

--- simplest-lambda/SimplestLambdaFunction/src/main/java/net/shyshkin/study/aws/serverless/simplest/App.java
+++ simplest-lambda/SimplestLambdaFunction/src/main/java/net/shyshkin/study/aws/serverless/simplest/App.java
@@ -1,12 +1,17 @@
 package net.shyshkin.study.aws.serverless.simplest;
 
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+
 /**
  * Handler for requests to Lambda function.
  */
-public class App {
+public class App implements RequestHandler<APIGatewayProxyRequestEvent, String> {
 
-    public String helloWorld(String name) {
-
+    @Override
+    public String handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+        String name = input.getHeaders().get("Art-User-Name");
         String message = "Hello " + name;
         System.out.println(message);
         return message;
