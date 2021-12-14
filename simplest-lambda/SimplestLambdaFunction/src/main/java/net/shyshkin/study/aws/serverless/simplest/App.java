9e4b094707dbf325ac859254f3d7c11af0f2a4d4
Art <d.art.shishkin@gmail.com>
2021-12-14 11:08:21 +0200

    30. Back to Earth (Hello World) (#1 Section 4: Create a Serverless Project)

---
+++ simplest-lambda/SimplestLambdaFunction/src/main/java/net/shyshkin/study/aws/serverless/simplest/App.java
@@ -0,0 +1,12 @@
+package net.shyshkin.study.aws.serverless.simplest;
+
+/**
+ * Handler for requests to Lambda function.
+ */
+public class App {
+
+    public void helloWorld() {
+        System.out.println("Hello World");
+    }
+
+}
