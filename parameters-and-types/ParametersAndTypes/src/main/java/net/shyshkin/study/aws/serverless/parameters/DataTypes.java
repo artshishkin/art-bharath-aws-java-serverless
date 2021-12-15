8ca100311c86a2a5631ce8a4babc7bd5b4f1343e
Art <d.art.shishkin@gmail.com>
2021-12-15 11:22:32 +0200

    42. Lists (#2)

--- parameters-and-types/ParametersAndTypes/src/main/java/net/shyshkin/study/aws/serverless/parameters/DataTypes.java
+++ parameters-and-types/ParametersAndTypes/src/main/java/net/shyshkin/study/aws/serverless/parameters/DataTypes.java
@@ -1,5 +1,10 @@
 package net.shyshkin.study.aws.serverless.parameters;
 
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 /**
  * Handler for requests to Lambda function.
  */
@@ -15,6 +20,21 @@ public class DataTypes {
 
     public Double getWrappedDouble(int number) {
         return (double) number;
+    }
+
+    public List<Integer> getScores() {
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+        return IntStream.range(0, 5)
+                .boxed()
+                .map(i -> random.nextInt(1, 100))
+                .collect(Collectors.toList());
+    }
+
+    public List<Integer> getScoresByName(List<String> names) {
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+        return names.stream()
+                .map(name->random.nextInt(1, 100))
+                .collect(Collectors.toList());
     }
 
 }
