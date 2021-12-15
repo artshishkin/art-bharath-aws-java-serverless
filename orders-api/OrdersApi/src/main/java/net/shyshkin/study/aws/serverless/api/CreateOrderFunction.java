e239f2d27b09eda1ea629378b4fe8605d89214a3
Art <d.art.shishkin@gmail.com>
2021-12-15 22:05:52 +0200

    61. Create Order Lambda (#3)

--- orders-api/OrdersApi/src/main/java/net/shyshkin/study/aws/serverless/api/CreateOrderFunction.java
+++ orders-api/OrdersApi/src/main/java/net/shyshkin/study/aws/serverless/api/CreateOrderFunction.java
@@ -4,45 +4,36 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.shyshkin.study.aws.serverless.api.model.Order;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
 
-/**
- * Handler for requests to Lambda function.
- */
 public class CreateOrderFunction implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
+    @Override
     public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
-        Map<String, String> headers = new HashMap<>();
-        headers.put("Content-Type", "application/json");
-        headers.put("X-Custom-Header", "application/json");
 
+        ObjectMapper objectMapper = new ObjectMapper();
         APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
-                .withHeaders(headers);
-        try {
-            final String pageContents = this.getPageContents("https://checkip.amazonaws.com");
-            String output = String.format("{ \"message\": \"hello world\", \"location\": \"%s\" }", pageContents);
+                .withHeaders(Map.of("Content-Type", "application/json"));
 
-            return response
-                    .withStatusCode(200)
-                    .withBody(output);
-        } catch (IOException e) {
-            return response
-                    .withBody("{}")
+        try {
+            Order order = objectMapper.readValue(input.getBody(), Order.class);
+
+
+            response
+                    .withBody("{\"orderId\": \"" + order.id + "\"}")
+                    .withStatusCode(200);
+
+        } catch (JsonProcessingException e) {
+            context.getLogger().log("Exception happens: " + e.getMessage());
+            response.withBody("{\"message\": \"" + e.getMessage().replace("\"", "'") + "\"}")
                     .withStatusCode(500);
         }
+
+        return response;
     }
 
-    private String getPageContents(String address) throws IOException{
-        URL url = new URL(address);
-        try(BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
-            return br.lines().collect(Collectors.joining(System.lineSeparator()));
-        }
-    }
 }
