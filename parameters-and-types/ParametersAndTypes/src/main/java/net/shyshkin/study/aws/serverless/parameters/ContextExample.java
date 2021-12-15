fc5d22a0d9c6c73c55a3243e1e36788249a488f6
Art <d.art.shishkin@gmail.com>
2021-12-15 13:56:29 +0200

    47. Context Object (#2)

---
+++ parameters-and-types/ParametersAndTypes/src/main/java/net/shyshkin/study/aws/serverless/parameters/ContextExample.java
@@ -0,0 +1,25 @@
+package net.shyshkin.study.aws.serverless.parameters;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+
+public class ContextExample {
+
+    public String viewContext(Context context) {
+        LambdaLogger logger = context.getLogger();
+        logger.log("\nFunction name: " + context.getFunctionName());
+        logger.log("\nRequest ID: " + context.getAwsRequestId());
+        logger.log("\nRemainingTimeInMillis: " + context.getRemainingTimeInMillis());
+        logger.log("\nMemoryLimitInMB: " + context.getMemoryLimitInMB());
+        logger.log("\nClientContext().getEnvironment(): " +
+                (
+                        context.getClientContext() != null ?
+                                context.getClientContext().getEnvironment() :
+                                "absent"
+                )
+        );
+
+        return context.getLogGroupName();
+    }
+
+}
