69e70d9736a9c3a41c29f7ba43695a25a454c6e1
Art <d.art.shishkin@gmail.com>
2021-12-15 14:33:37 +0200

    50. Variables in lambdas (#2)

--- parameters-and-types/ParametersAndTypes/src/main/java/net/shyshkin/study/aws/serverless/parameters/Variables.java
+++ parameters-and-types/ParametersAndTypes/src/main/java/net/shyshkin/study/aws/serverless/parameters/Variables.java
@@ -2,6 +2,25 @@ package net.shyshkin.study.aws.serverless.parameters;
 
 public class Variables {
 
+    private Double instanceVariable = Math.random();
+    private static Double staticVariable = Math.random();
+
+    public Variables() {
+        System.out.println("Inside Constructor");
+    }
+
+    static {
+        System.out.println("Static Block Execution");
+    }
+
+    public void coldStartBasics() {
+        double localVariable = Math.random();
+        System.out.println("Instance Variable: " + instanceVariable +
+                "; Static Variable: " + staticVariable +
+                "; Local Variable: " + localVariable
+        );
+    }
+
     public String viewEnvironmentVariables() {
         String envVar1 = System.getenv("MY_ENV_VAR_1");
         System.out.println("MY_ENV_VAR_1 value is `" + envVar1 + "`");
