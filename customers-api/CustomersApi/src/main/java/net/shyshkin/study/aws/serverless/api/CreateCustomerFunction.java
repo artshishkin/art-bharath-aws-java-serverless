6f25378351728275b292c6dace258e8937116bcc
Art <d.art.shishkin@gmail.com>
2021-12-16 23:06:52 +0200

    Assignment 2.3: CreateCustomerFunction (#3, #5)

--- customers-api/CustomersApi/src/main/java/net/shyshkin/study/aws/serverless/api/CreateCustomerFunction.java
+++ customers-api/CustomersApi/src/main/java/net/shyshkin/study/aws/serverless/api/CreateCustomerFunction.java
@@ -4,6 +4,13 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.shyshkin.study.aws.serverless.api.model.Customer;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
+import software.amazon.awssdk.services.dynamodb.model.PutItemResponse;
 
 import java.util.Map;
 
@@ -12,14 +19,46 @@ import java.util.Map;
  */
 public class CreateCustomerFunction implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    private final DynamoDbClient ddbClient = DynamoDbClient.builder()
+            .build();
+
+    private final String tableName = System.getenv("CUSTOMER_TABLE");
+
     public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
         Map<String, String> headers = Map.of("Content-Type", "application/json");
 
         APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
                 .withHeaders(headers);
 
+        try {
+            Customer customer = objectMapper.readValue(input.getBody(), Customer.class);
+
+            PutItemRequest putItemRequest = PutItemRequest.builder()
+                    .tableName(tableName)
+                    .item(Map.of(
+                            "id", AttributeValue.builder().n(String.valueOf(customer.id)).build(),
+                            "firstName", AttributeValue.builder().s(customer.firstName).build(),
+                            "lastName", AttributeValue.builder().s(customer.lastName).build(),
+                            "rewardPoints", AttributeValue.builder().n(String.valueOf(customer.rewardPoints)).build()
+                    ))
+                    .build();
+            PutItemResponse putItemResponse = ddbClient.putItem(putItemRequest);
+
+            int statusCode = putItemResponse.sdkHttpResponse().statusCode();
+
+            response
+                    .withBody("{\"customerId\": \"" + customer.id + "\"}")
+                    .withStatusCode(statusCode);
+
+        } catch (JsonProcessingException e) {
+            context.getLogger().log("Exception happens: " + e.getMessage());
+            response.withBody("{\"message\": \"" + e.getMessage().replace("\"", "'") + "\"}")
+                    .withStatusCode(500);
+        }
+
         return response;
     }
-
 
 }
