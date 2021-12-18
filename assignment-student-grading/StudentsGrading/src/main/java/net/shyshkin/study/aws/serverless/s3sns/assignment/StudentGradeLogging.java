fd76a0af90d4dbe7c8e7af1cfaa81250f0effa84
Art <d.art.shishkin@gmail.com>
2021-12-18 13:31:43 +0200

    Assignment 3.2: Test Lambda in the Console (#8)

--- assignment-student-grading/StudentsGrading/src/main/java/net/shyshkin/study/aws/serverless/s3sns/assignment/StudentGradeLogging.java
+++ assignment-student-grading/StudentsGrading/src/main/java/net/shyshkin/study/aws/serverless/s3sns/assignment/StudentGradeLogging.java
@@ -3,11 +3,15 @@ package net.shyshkin.study.aws.serverless.s3sns.assignment;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SNSEvent;
+import com.google.gson.Gson;
+import net.shyshkin.study.aws.serverless.s3sns.assignment.model.StudentWithGrade;
 
 /**
  * Handler for requests to Lambda function.
  */
 public class StudentGradeLogging implements RequestHandler<SNSEvent, Void> {
+
+    private final Gson gson = new Gson();
 
     @Override
     public Void handleRequest(SNSEvent input, Context context) {
@@ -15,7 +19,8 @@ public class StudentGradeLogging implements RequestHandler<SNSEvent, Void> {
         input.getRecords()
                 .stream()
                 .map(record -> record.getSNS().getMessage())
-                .forEach(message -> context.getLogger().log("Received message: " + message));
+                .map(message -> gson.fromJson(message, StudentWithGrade.class))
+                .forEach(studentWithGrade -> context.getLogger().log("Received student grade update: " + studentWithGrade));
 
         return null;
     }
