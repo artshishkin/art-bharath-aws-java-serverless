3cb3910e4dd50c58f9a7d9e8e82902a43c4bd1f0
Art <d.art.shishkin@gmail.com>
2021-12-18 20:50:36 +0200

    92. Using Lambda Context Logger (#9)

--- patient-checkout/PatientCheckout/src/main/java/net/shyshkin/study/aws/serverless/s3sns/PatientCheckoutLambda.java
+++ patient-checkout/PatientCheckout/src/main/java/net/shyshkin/study/aws/serverless/s3sns/PatientCheckoutLambda.java
@@ -1,6 +1,7 @@
 package net.shyshkin.study.aws.serverless.s3sns;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
 import com.amazonaws.services.s3.AmazonS3;
@@ -15,6 +16,8 @@ import net.shyshkin.study.aws.serverless.s3sns.model.PatientCheckoutEvent;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -28,9 +31,12 @@ public class PatientCheckoutLambda implements RequestHandler<S3Event, Void> {
     private final AmazonS3 s3 = AmazonS3ClientBuilder.defaultClient();
     private final AmazonSNS sns = AmazonSNSClientBuilder.defaultClient();
     private final String topicArn = System.getenv("PATIENT_CHECKOUT_TOPIC");
+    private LambdaLogger logger;
 
     @Override
     public Void handleRequest(S3Event input, Context context) {
+
+        logger = context.getLogger();
 
         input.getRecords().stream()
                 .map(record -> s3.getObject(
@@ -39,24 +45,27 @@ public class PatientCheckoutLambda implements RequestHandler<S3Event, Void> {
                 .map(S3Object::getObjectContent)
                 .map(this::toPatientCheckoutEvents)
                 .flatMap(Collection::stream)
-                .peek(patientCheckoutEvent -> context.getLogger().log(patientCheckoutEvent.toString()))
+                .peek(patientCheckoutEvent -> logger.log(patientCheckoutEvent.toString()))
                 .map(this::toJson)
                 .filter(Objects::nonNull)
-                .peek(message -> System.out.println("Published to SNS: " + message))
+                .peek(message -> logger.log("Published to SNS: " + message))
                 .map(json -> sns.publish(topicArn, json))
-                .forEach(publishResult -> context.getLogger().log(publishResult.toString()));
+                .forEach(publishResult -> logger.log(publishResult.toString()));
         return null;
     }
 
     private List<PatientCheckoutEvent> toPatientCheckoutEvents(InputStream s3InputStream) {
         try {
-            System.out.println("Reading data from S3");
+            logger.log("Reading data from S3");
             var patientCheckoutEvents = objectMapper.readValue(s3InputStream, checkoutEventsType);
-            System.out.println(patientCheckoutEvents);
+            logger.log(patientCheckoutEvents.toString());
             s3InputStream.close();
             return patientCheckoutEvents;
         } catch (IOException e) {
-            e.printStackTrace();
+//            logger.log(Arrays.toString(e.getStackTrace()));
+            StringWriter stringWriter = new StringWriter();
+            e.printStackTrace(new PrintWriter(stringWriter));
+            logger.log(stringWriter.toString());
             return new ArrayList<>();
         }
     }
@@ -65,7 +74,9 @@ public class PatientCheckoutLambda implements RequestHandler<S3Event, Void> {
         try {
             return objectMapper.writeValueAsString(object);
         } catch (JsonProcessingException e) {
-            e.printStackTrace();
+            StringWriter stringWriter = new StringWriter();
+            e.printStackTrace(new PrintWriter(stringWriter));
+            logger.log(stringWriter.toString());
             return null;
         }
     }
